09485e31573eeeba3eb1ec5e7e66630698978389
Paul Longo <paullongo@outlook.com>
2026-08-31 18:01:46 -0400

    Added some seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Lead;
+use App\Models\Sale;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,8 +21,13 @@ class DatabaseSeeder extends Seeder
         $this->call(LookupSeeder::class);
 
         User::factory()->admin()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Paul Longo',
+            'email' => 'paullongo@outlook.com',
+            'password' => Hash::make('Password123'),
         ]);
+
+        // Create some sample leads and sales
+        Lead::factory(20)->create();
+        Sale::factory(10)->create();
     }
 }
